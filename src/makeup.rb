983ae4386a9f747e60c9470ff6815d0194d8b751
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-10-09 13:07:32 +0200

    Solucionando error, #14

--- src/makeup.rb
+++ src/makeup.rb
@@ -7,6 +7,26 @@ class Maquillaje
     @tono = tono
     @preciorebajado
   end
+
+  #[HU04] - Consultar el precio rebajado #13
+  def consultarPrecio()  
+
+  end
+
+  #[HU03] - Consultar tonos disponibles #12
+  def consultarTonos()
+
+  end
+
+  #[HU02] - Poder listar todas las características de un producto #10
+  def listarCaracteristicas()
+
+  end
+
+  #[HU01] - Consultar la categoria de un producto #9
+  def consultarCategoria()
+
+  end
 end
 
 obj = Maquillaje.new('lipStain','labiales','17','8')
