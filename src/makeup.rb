cbf0ac9298c909999999f859053e4bce336867c6
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-10-12 17:35:54 +0200

    Actualizacion funcion consultarTono #12

--- src/makeup.rb
+++ src/makeup.rb
@@ -1,32 +1,29 @@
 class Maquillaje
   attr_reader :nombre, :categoria, :tono, :preciorebajado
   #inicialización de la clase
-  def initialize(nombre, categoria, tono, preciorebajado)
+  def initialize(nombre, categoria, tonos, preciorebajado)
     @nombre = nombre
     @categoria = categoria
-    @tono = tono
-    @preciorebajado
+    @tonos = tonos
+    @preciorebajado = preciorebajado
   end
 
-  #[HU04] - Consultar el precio rebajado #13
-  def consultarPrecio()  
+
+  def consultarPrecio()
 
   end
 
-  #[HU03] - Consultar tonos disponibles #12
   def consultarTonos()
 
   end
 
-  #[HU02] - Poder listar todas las características de un producto #10
   def listarCaracteristicas()
 
   end
 
-  #[HU01] - Consultar la categoria de un producto #9
   def consultarCategoria()
 
   end
 end
 
-obj = Maquillaje.new('lipStain','labiales','17','8')
+#obj = Maquillaje.new('lipStain','labiales','17','8')
