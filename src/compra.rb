30d0fd30e254adf40ddb8097720cf27588900d19
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-11-06 19:58:34 +0100

    corrijo fallo quitarCesta, #42

--- src/compra.rb
+++ src/compra.rb
@@ -15,7 +15,7 @@ class Compra
   end
 
   def quitarCesta(producto)
-    if(@cesta.include?(producto)){
+    if(@cesta.include?(producto))
       indice = @cesta.index(producto)
       @cesta.delete_at(indice);
     else
@@ -32,10 +32,4 @@ class Compra
     return total
   end
 
-  comp1 = Compra.new('00001')
-  obj = Maquillaje.new('prueba',[4, 5, 6, 7],10.0,5.0,[3, 2, 1, 7],TipoProducto::LABIOS,[['maria15','labios30'],[15,30]])
-  obj2 = Maquillaje.new('hola',[1, 2, 3, 4],10.0,5.0,[3, 4, 2, 3],TipoProducto::LABIOS,[['maria15','labios30'],[15,30]])
-  comp1.anadirCesta(obj).consultarPrecioDescontado()
-  comp1.anadirCesta(obj2).consultarPrecioDescontado()
-  comp1.calcularPrecioTotal()
 end
