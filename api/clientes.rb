7521e4860fc74a3b9ed4f269f681a2999bc35f28
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-11-21 20:24:01 +0100

    Añado funcion serverless clientes, #45

---
+++ api/clientes.rb
@@ -0,0 +1,35 @@
+require 'json'
+
+def leerDatos()
+  file = open("/api/clientes.json")
+  json = file.read
+  clientes = JSON.parse(json)
+  return clientes
+end
+
+def datosCliente(correo)
+
+  cad = 'Usuario no encontrado'
+  for i in (0..clientes["clientes"].length-1)
+    if clientes["clientes"][i]["correo"] == correo
+      cad = " Mis Datos Personales:
+      Nombre: #{clientes["clientes"][i]["nombre"]}
+      Apellidos: #{clientes["clientes"][i]["apellidos"]}
+      Correo Electrónico: #{correo}
+      Código Postal: #{clientes["clientes"][i]["codigoPostal"]}
+      Fecha de Nacimiento: #{clientes["clientes"][i]["FechaNacimiento"]} \n"
+    end
+  end
+  return cad
+end
+
+Handler = Proc.new do |req, res|
+  correo = req.query['correo'] || 'mariasanz@correo.ugr.es'
+
+  clientes = leerDatos()
+  cad = datosCliente(correo)
+
+  res.status = 200
+  res['Content-Type'] = 'text/text; charset=utf-8'
+  res.body = cad.to_json
+end
