9b903c26184a4ae344768d2a232a7566f480c02a
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-12-09 14:08:55 +0100

    avanzo app, #50, [skip ci]

--- sinatra/myapp.rb
+++ sinatra/myapp.rb
@@ -1,7 +1,22 @@
 # myapp.rb
 
 require 'sinatra'
+require 'json'
 
-get '/' do
-  'Hello world!'
+require_relative '../src/almacen.rb'
+require_relative '../src/compra.rb'
+class MyApp < Sinatra::Base
+
+  before do
+    @almacen = Almacen.new
+  end
+
+  get '/' do
+    {:status => 'ok'}.to_json
+  end
+
+  error 404 do
+    content_type :json
+    {:status => 'Error: ruta no encontrada'}.to_json
+  end
 end
