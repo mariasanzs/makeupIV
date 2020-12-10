2439bcfbf29be1249b7b49879558a21733be90e1
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-12-10 18:07:57 +0100

    añado test HU06, #42 [skip ci]

--- spec/testMyapp.rb
+++ spec/testMyapp.rb
@@ -11,4 +11,14 @@ RSpec.describe 'MyApp' do
     MyApp
   end
 
+  describe "precios de la cesta" do
+    it 'precio cesta correcto' do
+      get '/preciocesta'
+      res = 0
+      respuesta = {:precioCesta => res}.to_json
+      expect(last_response.body).to eq (respuesta)
+      expect(last_response.content_type).to eq ('application/json')
+      expect(last_response).to be_ok
+    end
+  end
 end
