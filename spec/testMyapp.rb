11b7aaf7ad51d3b1d394463fd00ec10fdfc8f0e4
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-12-10 19:11:41 +0100

    añado test HU05, #32 [skip ci]

--- spec/testMyapp.rb
+++ spec/testMyapp.rb
@@ -55,6 +55,34 @@ RSpec.describe 'MyApp' do
     end
   end
 
+  describe "Canjear código de un producto" do
+    it 'Canjear código ok' do
+      get '/producto/prueba/canjearCodigo/maria15'
+      res = 4.25
+      respuesta = {:preciorebajado => res}.to_json
+      expect(last_response.body).to eq (respuesta)
+      expect(last_response.content_type).to eq ('application/json')
+      expect(last_response).to be_ok
+    end
+
+    it 'Canjear código error no existe producto' do
+      get '/producto/noexisto/canjearCodigo/maria15'
+      respuesta = {:status => 'Error: Este producto no está en el catálogo'}.to_json
+      expect(last_response.body).to eq (respuesta)
+      expect(last_response.content_type).to eq ('application/json')
+      expect(last_response.status).to eq (400)
+    end
+
+    it 'Canjear código error no existe código' do
+      get '/producto/prueba/canjearCodigo/noexisto'
+      respuesta = {:status => 'Error: Este código no es válido'}.to_json
+      expect(last_response.body).to eq (respuesta)
+      expect(last_response.content_type).to eq ('application/json')
+      expect(last_response.status).to eq (400)
+    end
+  end
+
+
   describe "precios de la cesta" do
     it 'preciocesta ok' do
       get '/preciocesta'
